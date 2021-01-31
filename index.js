7dd4e312d2143cf5a0b3a39653d722fb489453c5
FerPM <46204965+codigorojo@users.noreply.github.com>
2021-01-31 10:29:25 -0300

    Acá probé con path y OK. Ahora voy viendo otra...

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 "use strict";
 
-var express = require("express");
-var app = express();
-var router = express.Router();
+const express = require("express");
+const path = require("path");
+const app = express();
 
-router.get("/router", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+app.use(express.static("files"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello word from myExpress");
-});
+app
+  .route("/")
+  .get(function (req, res) {
+    res.sendFile(path.join(__dirname + "/form.html"));
+  })
 
-app.get("/json", (req, res) => {
-  res.json({
-    id: 1,
-    name: "Fer",
-    age: 56,
+  .post(function (req, res) {
+    let html =
+      "<html><head><meta charset='UTF-8'><meta name='viewport';  content='width=device-width, initial-scale=1.0'><title>Usuario</title></head><body><div class='ui container'><h3>Nombre:</h3>" +
+      req.body.name +
+      "<p><h3>Apellido:</h3>" +
+      req.body.lastName +
+      "<h3>Edad:</h3>" +
+      req.body.age +
+      "<h3>Celular:</h3>" +
+      req.body.mobileNumber +
+      "<h3>Nacionalidad:</h3>" +
+      req.body.contryOrigin +
+      "<h3>País de residencia:</h3>" +
+      req.body.countryResidence +
+      "<br><br><a class='ui button' href='/'>volver</a></p></div></body><html>";
+    res.send(html);
   });
-});
+
 app.listen(3000, () => {
   console.log("servidor 3000 corriendo...");
 });
-
-module.exports = router;

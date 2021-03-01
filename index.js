cbae0f6fbbeb656ecb2f0505704620e61382391a
FerPM <46204965+codigorojo@users.noreply.github.com>
2021-03-01 11:53:56 -0300

    una manera más básica

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/form", (req, res) => {
+app.get("/", (req, res) => {
+  res.send("ok, funciona!!");
+});
+
+app.post("/form01", (req, res) => {
+  let name = req.body.name;
+  let lastName = req.body.lastName;
+  let age = req.body.age;
+  let mobile = req.body.mobile;
+  let nacionality = req.body.nacionality;
+  let residence = req.body.residence;
+
   let html =
-    "<html><head><title>Registro</title></head><body><h1>Hola " +
-    req.body.name +
-    ", </h1><br/><h2>Los datos ingresados son: </h2>" +
-    "<ul><li><b>Nombre: </b>" +
-    req.body.name +
-    " " +
-    req.body.lastName +
-    "</li>" +
-    "<li><b>Edad: </b>" +
-    req.body.age +
-    "</li>" +
-    "<li><b>Tel. Celular: </b>" +
-    req.body.cel +
-    "</li>" +
-    "<li><b>Pais: </b>" +
-    req.body.country +
-    "</li>" +
-    "<li><b>Pais de residencia: </b>" +
-    req.body.residence +
-    "</li></ul>" +
-    "<p><a href=/index.html>Volver</a><p>" +
-    "</body><html></html>";
+    "El nombre es " +
+    name +
+    "<br>" +
+    "El apellido es " +
+    lastName +
+    "<br>" +
+    "El celular es " +
+    mobile +
+    "<br>" +
+    '<a href="/">volver</a>';
   res.send(html);
 });
 
 app.listen(3000, () => {
-  console.log("corriendo en el puerto 3000");
+  console.log("la aplicación esta corriendo");
 });
